1f56ea32c3a87334823200179e881201e8c6e76c
wangjiawei <cswangjiawei@163.com>
2021-03-02 15:45:16 +0800

    update 94 solution

--- C++/94.cpp
+++ C++/94.cpp
@@ -34,3 +34,37 @@ public:
         }
     }
 };
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> stk;
+        while(root || !stk.empty())
+        {
+            while (root)
+            {
+                stk.push(root);
+                root = root -> left;
+            }
+            root = stk.top();
+            stk.pop();
+            result.emplace_back(root -> val);
+            root = root -> right;
+        }
+        return result;
+    }
+};
