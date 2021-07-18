699b3a21bf5b1de6c6d40cbd3276007da88ef5e2
wangjiawei <cswangjiawei@163.com>
2021-07-18 15:47:07 +0800

    add 24th code

---
+++ C++/24.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* cur = head;
+        ListNode* pre = nullptr;
+        ListNode* new_head = head;
+        while (cur && cur->next)
+        {
+            if (cur == head) 
+            {
+                new_head = cur->next;
+            }
+            if (pre != nullptr)
+            {
+                pre->next = cur->next;
+            }
+            auto temp = cur->next;
+            cur->next = temp->next;
+            temp->next =cur;
+            pre = cur;
+            cur = cur->next;
+        }
+        return new_head;
+    }
+};
