04c985509f058a2b9ed42f8279c1fb85e0956be6
wangjiawei <cswangjiawei@163.com>
2021-07-25 21:03:04 +0800

    add 24th code(recursion)

--- C++/24.cpp
+++ C++/24.cpp
@@ -33,3 +33,29 @@ public:
         return new_head;
     }
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* new_head=head;
+        if (head && head->next)
+        {
+            new_head = head->next;
+            auto temp = new_head->next;
+            new_head->next = head;
+            head->next = swapPairs(temp);
+        }
+        return new_head;
+    }
+};
