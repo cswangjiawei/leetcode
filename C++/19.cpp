6ab2d03ff8f248f631b1998a6e98b2ffb50f2bd6
wangjiawei <cswangjiawei@163.com>
2022-10-04 16:26:40 +0800

    update:19th solution

--- C++/19.cpp
+++ C++/19.cpp
@@ -11,41 +11,22 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* head_1 = head;
-        ListNode* head_2 = nullptr;
-        ListNode* prev_head_2 = nullptr;
-        int i = 1;
-        while (head_1 != nullptr)
-        {
-            if (i == n)
-            {
-                head_2 = head;
-            }
-            else
-            {
-                if (head_2 != nullptr)
-                {
-                    prev_head_2 = head_2;
-                    head_2 = head_2->next;
-                }
-            }
-            if (head_1->next == nullptr)
-            {
-                if (prev_head_2 != nullptr)
-                {
-                    prev_head_2->next = head_2->next;
-                    head_2->next = nullptr;
-                }
-                else
-                {
-                    head = head->next;
-                    
-                }
-                break;
-            }
-            head_1 = head_1->next;
-            ++i;
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* fast = head;
+        ListNode* slow = dummy;
+
+        while (n) {
+            fast = fast->next;
+            --n;
         }
-        return head;
+
+        while(fast) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        slow->next = slow->next->next;
+        return dummy->next;
+
     }
 };
