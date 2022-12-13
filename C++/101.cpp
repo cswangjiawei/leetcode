5146fd40bd3e25f5fd78fc693664f46efb40ac8e
wangjiawei <cswangjiawei@163.com>
2022-12-13 17:04:36 +0800

    add 101th code

---
+++ C++/101.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(!root) {
+            return true;
+        }
+        return check(root->left, root->right);
+
+    }
+
+    bool check(TreeNode* root1, TreeNode* root2) {
+        if (root1 && root2) {
+            if (root1->val  != root2->val) {
+                return false;
+            }
+            return check(root1->right, root2->left) && check(root1->left, root2->right);
+        }
+        if (!root1 && !root2){
+            return true;
+        }
+        return false;
+    }
+};
