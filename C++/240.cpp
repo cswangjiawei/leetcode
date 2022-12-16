0566e60b44411c337909fc8be6b8debd1b6d5bb3
wangjiawei <cswangjiawei@163.com>
2022-12-16 11:16:46 +0800

    add 240th code

---
+++ C++/240.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int cloumn = matrix[0].size();
+
+        if(target > matrix[row-1][cloumn-1] || target < matrix[0][0]) {
+            return false;
+        }
+
+        int cur_row = 0;
+        while(cur_row < row) {
+            if(target >= matrix[cur_row][0] && target <= matrix[cur_row][cloumn-1]) {
+                int left = 0;
+                int right = cloumn-1;
+                while(left <= right) {
+                    int mid = (left + right) / 2;
+                    if (matrix[cur_row][mid] == target) {
+                        return true;
+                    }
+                    if(matrix[cur_row][mid] > target) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+            }
+            ++cur_row;
+        }
+        
+        return false;
+    }
+};
