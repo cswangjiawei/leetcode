d7868e570a63a1913a0f8c8f028d962abf310d79
wangjiawei <cswangjiawei@163.com>
2023-01-02 16:16:23 +0800

    add 283th code

---
+++ C++/283.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left = 0;
+        int right = 0;
+        while(right < nums.size()) {
+            if(nums[right]) {
+                swap(nums[left], nums[right]);
+                ++left;
+            }
+            ++right;
+        }
+        
+    }
+};
