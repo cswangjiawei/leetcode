44e3d19d0d9dc33040b3635006f06784523fb7b5
wangjiawei <cswangjiawei@163.com>
2023-01-18 10:59:21 +0800

    update 912.cpp

--- C++/912.cpp
+++ C++/912.cpp
@@ -1,67 +1,46 @@
-#include <vector>
-#include <iostream>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        int n = static_cast<int>(nums.size()) - 1;
-        quick_sort(nums, 0, n);
+        srand((unsigned)time(NULL));
+        int n = static_cast<int>(nums.size());
+        quick_sort(nums, 0, n-1);
         return nums;
     }
 
-    int partition(vector<int> &nums, int low, int high)
-    {
-        int pivotkey = nums[low];
-        while (low < high)
-        {
-            while (low < high)
-            {
-                if (nums[high] < pivotkey)
-                {
-                    nums[low] = nums[high];
-                    nums[high] = pivotkey;
-                    break;
-                }
-                else
-                {
+    int Partition(vector<int>& nums, int low, int high) {
+        int i = rand() % (high - low + 1) + low;
+        swap(nums[low], nums[i]);
+        int pivot_key = nums[low];
+        while (low < high) {
+            while (low < high) {
+                if (nums[high] >= pivot_key) {
                     --high;
+                } else {
+                    nums[low] = nums[high];
+                    nums[high] = pivot_key;
+                    break;
                 }
             }
-            while (low < high)
-            {
-                if (nums[low] > pivotkey)
-                {
-                    nums[high] = nums[low];
-                    nums[low] = pivotkey;
-                    break;
-                }
-                else
-                {
+
+            while (low < high) {
+                if (nums[low] <= pivot_key) {
                     ++low;
+                } else {
+                    nums[high] = nums[low];
+                    nums[low] = pivot_key;
+                    break;
                 }
             }
         }
-        nums[low] = pivotkey;
         return low;
     }
 
-    void quick_sort(vector<int> &nums, int low, int high)
-    {
-        if (low < high)
-        {
-            int pivot = partition(nums, low, high);
+    void quick_sort(vector<int>& nums, int low, int high) {
+        if (low<high) {
+
+            int pivot = Partition(nums, low, high);
             quick_sort(nums, low, pivot-1);
             quick_sort(nums, pivot+1, high);
         }
     }
 };
-
-int main()
-{
-    Solution solu;
-    vector<int> nums = {5,2,3,1};
-    solu.sortArray(nums);
-    return 0;
-}
