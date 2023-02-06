b4e674c537b6f3a8b5fc625bcf24befc288a7297
wangjiawei <cswangjiawei@163.com>
2023-02-06 10:46:33 +0800

    update 50th code

--- C++/50.cpp
+++ C++/50.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+       if (x == 0) {
+           return 0;
+       }
+
        if (n==0) {
            return 1;
        }
+
        if (n==1) {
            return x;
        }
 
-        long long k = n;
-       if (n < 0) {
-           k = -1 * k;
-       } else {
-           k = n;
+       if (n==-1) {
+           return 1.0/x;
        }
 
-       double y = myPow(x, k/2);
-       if (k%2 == 0) {
-           if (n > 0) {
-               return y * y;
-           } else {
-               return 1.0/(y*y);
-           }
+       
+
+       double tmp = myPow(x, n/2); 
+       double ans = 0;
+       if (n%2 == 0) {
+           ans = tmp * tmp;
        } else {
            if (n > 0) {
-               return y*y*x;
+               ans = tmp * tmp * x;
            } else {
-               return 1.0/(y*y*x);
+               ans = tmp * tmp * (1/x);
            }
-           
        }
+       return ans;
+
     }
 };
