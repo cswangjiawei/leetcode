f5740980294c90c2798eed3d9f10ab09461f7e55
wangjiawei <cswangjiawei@163.com>
2023-02-24 10:12:24 +0800

    update 617th code

--- C++/617.cpp
+++ C++/617.cpp
@@ -12,33 +12,18 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        int root1_val = 0;
-        int root2_val = 0;
-        TreeNode* root1_left = nullptr;
-        TreeNode* root2_left = nullptr;
-        TreeNode* root1_right = nullptr;
-        TreeNode* root2_right = nullptr;
-        if (root1 != nullptr) {
-            root1_val = root1->val;
-            root1_left = root1->left;
-            root1_right = root1->right;
-        }
-        if (root2 != nullptr) {
-            root2_val = root2->val;
-            root2_left = root2->left;
-            root2_right = root2->right;
+        if (root1 == nullptr) {
+            return root2;
         }
 
-        if (root1 || root2) {
-            TreeNode* new_root = new TreeNode(root1_val + root2_val);
-            new_root->left = mergeTrees(root1_left, root2_left);
-            new_root->right = mergeTrees(root1_right, root2_right);
-            return new_root;
-        } else {
-            return nullptr;
+        if (root2 == nullptr) {
+            return root1;
         }
 
+        TreeNode* new_root = new TreeNode(root1->val + root2->val);
+        new_root->left = mergeTrees(root1->left, root2->left);
+        new_root->right = mergeTrees(root1->right, root2->right);
 
-        
+        return new_root;
     }
 };
