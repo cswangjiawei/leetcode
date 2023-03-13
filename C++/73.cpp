b625d3305da0f1490f32d5315cadb416077fdc30
wangjiawei <cswangjiawei@163.com>
2023-03-13 17:21:06 +0800

    add 73th code

---
+++ C++/73.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = static_cast<int>(matrix.size());
+        int n = static_cast<int>(matrix[0].size());
+
+        vector<bool> row(m, false);
+        vector<bool> col(n, false);
+
+        for (int i=0; i<m; ++i) {
+            for (int j=0; j<n; ++j) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        for (int i=0; i<m; ++i) {
+            for (int j=0; j<n; ++j) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+};
