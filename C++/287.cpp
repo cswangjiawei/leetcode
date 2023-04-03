5aacb60093c31d63baf5a736625b69a0dda041aa
wangjiawei <cswangjiawei@163.com>
2023-04-03 15:01:49 +0800

    add 287th code

---
+++ C++/287.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0;
+        int fast = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[fast];
+            fast = nums[fast];
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        slow = 0;
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
