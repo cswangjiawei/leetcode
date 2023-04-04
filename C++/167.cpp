ea4ac72c4c3d92bd5e0ccb7782cb61b109349409
wangjiawei <cswangjiawei@163.com>
2023-04-04 16:47:12 +0800

    add 167th code

---
+++ C++/167.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = static_cast<int>(numbers.size());
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                return {left+1, right+1};
+            }
+            if (numbers[left] + numbers[right] > target) {
+                --right;
+            } else {
+                ++left;
+            }
+        }
+        return {};
+    }
+};
