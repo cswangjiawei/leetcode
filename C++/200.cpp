79f83f5269138721e91b90d41fdce3646756c439
wangjiawei <cswangjiawei@163.com>
2023-04-23 10:33:13 +0800

    update 200th code

--- C++/200.cpp
+++ C++/200.cpp
@@ -31,3 +31,80 @@ public:
         }
     }
 };
+
+
+class UnionFind {
+private:
+    int count;
+    vector<int> parent;
+    vector<int> rank;
+
+public:
+    UnionFind(vector<vector<char>>& grid):count(0) {
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int i=0; i<m; ++i) {
+            for (int j=0; j<n; ++j) {
+                if (grid[i][j] == '1') {
+                    parent.emplace_back(i*n + j);
+                    ++count;
+                } else {
+                    parent.emplace_back(-1);
+                }
+                rank.emplace_back(0);
+            }
+        }
+    }
+
+    int find(int i) {
+        if (i != parent[i]) {
+            parent[i] = find(parent[i]);
+        }
+        return parent[i];
+    }
+
+    void unite(int x, int y) {
+        int parent_x = find(x);
+        int parent_y = find(y);
+        if (parent_x != parent_y) {
+            --count;
+            if (rank[parent_x] < rank[parent_y]) {
+                swap(rank[parent_x], rank[parent_y]);
+            }
+            parent[parent_y] = parent_x;
+            if (rank[parent_x] == rank[parent_y]) {
+                rank[parent_x] += 1;
+            }
+        }
+    }
+
+    int get_count() const {
+        return count;
+    }
+
+};
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int row = grid.size();
+        if (row==0) return 0;
+        int col = grid[0].size();
+
+        UnionFind uf(grid);
+        for (int i=0; i<row; ++i) {
+            for (int j=0; j<col; ++j) {
+                if (grid[i][j] == '1') {
+                    grid[i][j] = '0';
+                    if (i-1>=0 && grid[i-1][j]=='1') uf.unite(i*col+j, (i-1)*col+j);
+                    if (i+1<row && grid[i+1][j]=='1') uf.unite(i*col+j, (i+1)*col+j);
+                    if (j-1>=0 && grid[i][j-1]=='1') uf.unite(i*col+j, i*col+j-1);
+                    if (j+1<col && grid[i][j+1]=='1') uf.unite(i*col+j, i*col+j+1);
+                }
+            }
+        }
+
+        return uf.get_count();
+
+    }
+};
