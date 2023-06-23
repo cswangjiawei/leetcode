34079d8c26cf99a8b4d7a161857f543d852cfb75
wangjiawei <cswangjiawei@163.com>
2023-06-23 20:56:50 +0800

    add 435th code

---
+++ C++/435.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), [] (vector<int>& a, vector<int>& b) {return a[1] < b[1];});
+        int ans=1;
+        int right = intervals[0][1];
+        for (int i=1; i<n; ++i) {
+            if (intervals[i][0] >= right) {
+                ++ans;
+                right = intervals[i][1];
+            }
+        }
+        return n - ans;
+    }
+};
