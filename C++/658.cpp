01bb8ad1038ddfc480017a28139771f5df138bfd
wangjiawei <cswangjiawei@163.com>
2023-09-02 17:36:01 +0800

    add 658th code

--- C++/658.cpp
+++ C++/658.cpp
@@ -25,3 +25,27 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int right = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
+        int left = right - 1;
+        while (k) {
+            if (left < 0) {
+                ++right;
+            } else if (right >= arr.size()) {
+                --left;
+            } else if (abs(arr[right]-x) < abs(arr[left] - x)) {
+                ++right;
+            } else {
+                --left;
+            }
+            --k;
+        }
+        return {arr.begin()+left+1, arr.begin()+right};
+
+        
+    }
+};
