6aeb6ec7343d26e62b1ddff8ae6e34cb5dd413d1
wangjiawei <cswangjiawei@163.com>
2025-04-18 07:49:12 +0800

    update 74th code

--- C++/74.cpp
+++ C++/74.cpp
@@ -24,3 +24,31 @@ public:
         return false;
     }
 };
+
+
+class Solution {
+    public:
+        bool searchMatrix(vector<vector<int>>& matrix, int target) {
+            int m = matrix.size();
+            int n = matrix[0].size();
+    
+            int left = 0;
+            int right = m * n - 1;
+    
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                int i = mid / n;
+                int j = mid % n;
+                if (matrix[i][j] == target) {
+                    return true;
+                } else {
+                    if (matrix[i][j] > target) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+            }
+            return false;
+        }
+    };
