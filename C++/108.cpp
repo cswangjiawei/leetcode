c547f95ac1114b428f364a90f4753f9954f3c39d
wangjiawei <cswangjiawei@163.com>
2025-12-07 16:02:34 +0800

    update 108th code

--- C++/108.cpp
+++ C++/108.cpp
@@ -28,3 +28,36 @@ public:
         return root;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.size() == 0)
+        {
+            return nullptr;
+        }
+        TreeNode* ans;
+        int n = nums.size();
+        int mid = n/2;
+        ans = new TreeNode(nums[mid]);
+        vector<int> left_nums = vector(nums.begin(), nums.begin() + mid);
+        ans->left = sortedArrayToBST(left_nums);
+        vector<int> right_nums = vector(nums.begin()+mid+1, nums.end());
+        ans->right = sortedArrayToBST(right_nums);
+
+        return ans;
+
+    }
+};
